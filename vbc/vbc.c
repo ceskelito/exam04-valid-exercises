70987cfffd1c15aab527be6f8d72fd758b39561b
ceske-42 <ceske@etik.com>
2026-03-15 18:36:19 +0100

    Removed commented functions

--- vbc/vbc.c
+++ vbc/vbc.c
@@ -13,27 +13,6 @@ typedef struct node {
     struct node *r;
 }   node;
 
-// node    *new_node(node n)
-// {
-//     node *ret = calloc(1, sizeof(n));
-//     if (!ret)
-//         return (NULL);
-//     *ret = n;
-//     return (ret);
-// }
-//
-// void    destroy_tree(node *n)
-// {
-//     if (!n)
-//         return ;
-//     if (n->type != VAL)
-//     {
-//         destroy_tree(n->l);
-//         destroy_tree(n->r);
-//     }
-//     free(n);
-// }
-//
 void    unexpected(char c)
 {
     if (c)
@@ -41,51 +20,6 @@ void    unexpected(char c)
     else
         printf("Unexpected end of input\n");
 }
-//
-// int accept(char **s, char c)
-// {
-//     if (**s)
-//     {
-//         (*s)++;
-//         return (1);
-//     }
-//     return (0);
-// }
-//
-// int expect(char **s, char c)
-// {
-//     if (accept(s, c))
-//         return (1);
-//     unexpected(**s);
-//     return (0);
-// }
-//
-// //...
-//
-// node    *parse_expr(char *s)
-// {
-//     //...
-//
-//     if (*s) 
-//     {
-//         destroy_tree(ret);
-//         return (NULL);
-//     }
-//     return (ret);
-// }
-//
-// int eval_tree(node *tree)
-// {
-//     switch (tree->type)
-//     {
-//         case ADD:
-//             return (eval_tree(tree->l) + eval_tree(tree->r));
-//         case MULTI:
-//             return (eval_tree(tree->l) * eval_tree(tree->r));
-//         case VAL:
-//             return (tree->val);
-//     }
-// }
 
 #include <limits.h>
 #include <stdbool.h>
@@ -221,9 +155,4 @@ int main(int argc, char **argv)
 	if ( validate_input(argv[1]) == -1 )
 		return(1);
 	printf("%d\n", n(argv[1]));
-    // node *tree = parse_expr(argv[1]);
-    // if (!tree)
-    //     return (1);
-    // printf("%d\n", eval_tree(tree));
-    // destroy_tree(tree);
 }
