8bfda0e8fd72e9c0043669a538ae94f9894109c9
ceske-42 <ceske@etik.com>
2026-03-15 17:45:18 +0100

    vbc exam version

---
+++ vbc/old.vbc.c
@@ -0,0 +1,216 @@
+/**
+ * Assignment name  : vbc
+ * Expected files   : *.c *.h
+ * Allowed functions: malloc, calloc, realloc, free, printf, isdigit, write
+ * --------------------------------------------------------------------------------
+ * 
+ * Write a program that will print the result of a math expression given as
+ * argument.
+ * You must handle the operations + * and the parenthesis.
+ * You don't have to handle whitespaces in the expression.
+ * All the values in the expression will be between 0 and 9 included.
+ * In case of unexpected symbol or inadepthropriate parenthesis, you will
+ * print "Unexpected token '%c'
+ * " and exit with the code 1 (if the
+ * symbol is the end of input you will print: "Unexpected end of input
+ * ").
+ * In case of a syscall failure you will just exit with the code 1.
+ * 
+ * You will find in this directory the beginning of the code you need to write.
+ */
+
+#include <stdio.h>
+#include <malloc.h> // change this to <stdlib.h>
+#include <ctype.h>
+#include <limits.h>
+
+enum { ERR = 1, NICE = 0};
+
+typedef struct node {
+    enum {
+        ADD,
+        MULTI,
+        VAL
+    }   type;
+    int val;
+    struct node *l;
+    struct node *r;
+}   node;
+
+node *n(char **s);
+
+int	validate_input(char *s) {
+
+	if ( !isdigit(s[0]) || s[0] != '(' )
+		return ERR;
+	for ( int i = 0; s[i]; i++ ) {
+
+		if ( s[i] == '(' || s[i] == '+' || s[i] == '*' ) {
+			if ( !s[i + 1] || 
+					!( isdigit(s[i + 1]) || s[i + 1] == '(' )) {
+				return ERR;	
+			}
+		} else if ( s[i] == ')' || isdigit(s[i]) ) {
+			if ( s[i + 1] && 
+					!( s[i + 1] == '(' || s[i + 1] == '+' || s[i + 1] == '*' ) ) {
+				return ERR;
+			}
+		} else
+			return ERR;
+	}
+	return NICE;
+}
+
+node    *new_node(node n)
+{
+    node *ret = calloc(1, sizeof(n));
+    if (!ret)
+        return (NULL);
+    *ret = n;
+    return (ret);
+}
+
+void    destroy_tree(node *n)
+{
+    if (!n)
+        return ;
+    if (n->type != VAL)
+    {
+        destroy_tree(n->l);
+        destroy_tree(n->r);
+    }
+    free(n);
+}
+
+void    unexpected(char c)
+{
+    if (c)
+        printf("Unexpected token '%c'\n", c);
+    else
+        printf("Unexpected end of input\n");
+}
+
+int accept(char **s, char c)
+{
+    if (**s)
+    {
+        (*s)++;
+        return (1);
+    }
+    return (0);
+}
+
+int expect(char **s, char c)
+{
+    if (accept(s, c))
+        return (1);
+    unexpected(**s);
+    return (0);
+}
+
+//...
+
+// node    *parse_expr(char *s)
+// {
+//     //...
+//
+//     if (*s) 
+//     {
+//         destroy_tree(ret);
+//         return (NULL);
+//     }
+//     return (ret);
+// }
+
+int eval_tree(node *tree)
+{
+    switch (tree->type)
+    {
+        case ADD:
+            return (eval_tree(tree->l) + eval_tree(tree->r));
+        case MULTI:
+            return (eval_tree(tree->l) * eval_tree(tree->r));
+        case VAL:
+            return (tree->val);
+    }
+}
+
+int get_type( char c ) {
+	if ( c == '+' )
+		return ADD;
+	if ( c == '*' )
+		return MULTI;
+	if ( isdigit(c) )
+		return VAL;
+	return ( -1 );
+}
+
+enum { PAR_PRIORITY = 10 };
+
+#define CALC_PRIORITY(type, depth) ( (type) + (depth * PAR_PRIORITY) )
+
+node *n(char **s) {
+
+	if ( !s || !*s )
+		return NULL;
+
+	struct expression_element {
+		int type;	// Element type (val, sum, multi). Also encodes the operator precedence (excluding parentheses).
+		int priority; // Effective precedence, derived from the type and the current parentheses depth.
+		char *ptr; // Pointer to the element in the original expression's string.
+	} curr, min_prior;
+	
+	min_prior.priority = INT_MAX;
+	min_prior.type = VAL;
+	min_prior.ptr = 0;
+	
+	node new;
+
+	int depth = 0; // Parenthesis depth
+
+	// Find the less prioritized node (including parenthesis)
+	for ( int i = 0; (*s)[i]; i++ ) {
+
+		if ( (*s)[i] == '(' ){
+			depth++;
+			continue;
+		}
+		else if ( (*s)[i] == ')' ) {
+			depth--;
+			continue;
+		}
+
+		curr.type = get_type((*s)[i]);
+		curr.priority = CALC_PRIORITY(curr.type, depth);
+		curr.ptr = &(*s)[i];
+
+		if ( curr.type != -1 && curr.priority < min_prior.priority )
+			min_prior = curr;
+	}
+
+	new.type = min_prior.type;
+	if ( new.type == VAL ) {
+		new.val = min_prior.ptr[0] - '0';
+		return (new_node(new));
+	}
+	*min_prior.ptr = 0;
+	char *right_s = min_prior.ptr + 1;
+	if ( right_s )
+		new.r = n(&right_s);
+	new.l = n(s);
+	
+	return new_node(new);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+        return (1);
+	// if ( validate_input(argv[1]) == ERR )
+	// 	return ERR;
+    node *tree = n(&argv[1]);//parse_expr(argv[1]);
+    if (!tree)
+        return (1);
+    printf("%d\n", eval_tree(tree));
+    destroy_tree(tree);
+}
--- vbc/vbc.c
+++ vbc/vbc.c
@@ -1,30 +1,6 @@
-/**
- * Assignment name  : vbc
- * Expected files   : *.c *.h
- * Allowed functions: malloc, calloc, realloc, free, printf, isdigit, write
- * --------------------------------------------------------------------------------
- * 
- * Write a program that will print the result of a math expression given as
- * argument.
- * You must handle the operations + * and the parenthesis.
- * You don't have to handle whitespaces in the expression.
- * All the values in the expression will be between 0 and 9 included.
- * In case of unexpected symbol or inadepthropriate parenthesis, you will
- * print "Unexpected token '%c'
- * " and exit with the code 1 (if the
- * symbol is the end of input you will print: "Unexpected end of input
- * ").
- * In case of a syscall failure you will just exit with the code 1.
- * 
- * You will find in this directory the beginning of the code you need to write.
- */
-
 #include <stdio.h>
 #include <malloc.h> // change this to <stdlib.h>
 #include <ctype.h>
-#include <limits.h>
-
-enum { ERR = 1, NICE = 0};
 
 typedef struct node {
     enum {
@@ -37,79 +13,55 @@ typedef struct node {
     struct node *r;
 }   node;
 
-node *n(char **s);
-
-int	validate_input(char *s) {
-
-	if ( !isdigit(s[0]) || s[0] != '(' )
-		return ERR;
-	for ( int i = 0; s[i]; i++ ) {
-
-		if ( s[i] == '(' || s[i] == '+' || s[i] == '*' ) {
-			if ( !s[i + 1] || 
-					!( isdigit(s[i + 1]) || s[i + 1] == '(' )) {
-				return ERR;	
-			}
-		} else if ( s[i] == ')' || isdigit(s[i]) ) {
-			if ( s[i + 1] && 
-					!( s[i + 1] == '(' || s[i + 1] == '+' || s[i + 1] == '*' ) ) {
-				return ERR;
-			}
-		} else
-			return ERR;
-	}
-	return NICE;
-}
-
-node    *new_node(node n)
-{
-    node *ret = calloc(1, sizeof(n));
-    if (!ret)
-        return (NULL);
-    *ret = n;
-    return (ret);
-}
-
-void    destroy_tree(node *n)
-{
-    if (!n)
-        return ;
-    if (n->type != VAL)
-    {
-        destroy_tree(n->l);
-        destroy_tree(n->r);
-    }
-    free(n);
-}
-
-void    unexpected(char c)
-{
-    if (c)
-        printf("Unexpected token '%c'\n", c);
-    else
-        printf("Unexpected end of input\n");
-}
-
-int accept(char **s, char c)
-{
-    if (**s)
-    {
-        (*s)++;
-        return (1);
-    }
-    return (0);
-}
-
-int expect(char **s, char c)
-{
-    if (accept(s, c))
-        return (1);
-    unexpected(**s);
-    return (0);
-}
-
-//...
-
+// node    *new_node(node n)
+// {
+//     node *ret = calloc(1, sizeof(n));
+//     if (!ret)
+//         return (NULL);
+//     *ret = n;
+//     return (ret);
+// }
+//
+// void    destroy_tree(node *n)
+// {
+//     if (!n)
+//         return ;
+//     if (n->type != VAL)
+//     {
+//         destroy_tree(n->l);
+//         destroy_tree(n->r);
+//     }
+//     free(n);
+// }
+//
+// void    unexpected(char c)
+// {
+//     if (c)
+//         printf("Unexpected token '%c'\n", c);
+//     else
+//         printf("Unexpected end of input\n");
+// }
+//
+// int accept(char **s, char c)
+// {
+//     if (**s)
+//     {
+//         (*s)++;
+//         return (1);
+//     }
+//     return (0);
+// }
+//
+// int expect(char **s, char c)
+// {
+//     if (accept(s, c))
+//         return (1);
+//     unexpected(**s);
+//     return (0);
+// }
+//
+// //...
+//
 // node    *parse_expr(char *s)
 // {
 //     //...
@@ -121,96 +73,145 @@ int expect(char **s, char c)
 //     }
 //     return (ret);
 // }
+//
+// int eval_tree(node *tree)
+// {
+//     switch (tree->type)
+//     {
+//         case ADD:
+//             return (eval_tree(tree->l) + eval_tree(tree->r));
+//         case MULTI:
+//             return (eval_tree(tree->l) * eval_tree(tree->r));
+//         case VAL:
+//             return (tree->val);
+//     }
+// }
 
-int eval_tree(node *tree)
-{
-    switch (tree->type)
-    {
-        case ADD:
-            return (eval_tree(tree->l) + eval_tree(tree->r));
-        case MULTI:
-            return (eval_tree(tree->l) * eval_tree(tree->r));
-        case VAL:
-            return (tree->val);
-    }
-}
+#include <limits.h>
+#include <stdbool.h>
 
-int get_type( char c ) {
-	if ( c == '+' )
-		return ADD;
-	if ( c == '*' )
-		return MULTI;
-	if ( isdigit(c) )
-		return VAL;
-	return ( -1 );
-}
+enum { PAR_WEIGHT = 10 };
 
-enum { PAR_PRIORITY = 10 };
+#define GET_PRIOR(weight, depth) ( weight + (depth * PAR_WEIGHT) )
 
-#define CALC_PRIORITY(type, depth) ( (type) + (depth * PAR_PRIORITY) )
+int n(char *s) {
 
-node *n(char **s) {
-
-	if ( !s || !*s )
-		return NULL;
-
-	struct expression_element {
-		int type;	// Element type (val, sum, multi). Also encodes the operator precedence (excluding parentheses).
-		int priority; // Effective precedence, derived from the type and the current parentheses depth.
-		char *ptr; // Pointer to the element in the original expression's string.
+	int	res;
+	struct s_elem {
+		int		val;
+		int		depth;
+		int		weight;
+		int		priority;
+		char	*ptr;
 	} curr, min_prior;
-	
+
 	min_prior.priority = INT_MAX;
-	min_prior.type = VAL;
-	min_prior.ptr = 0;
-	
-	node new;
+	min_prior.weight = VAL + 1;
+	min_prior.val = 0;
+	min_prior.ptr = NULL;
 
-	int depth = 0; // Parenthesis depth
+	curr.depth = 0;
+	curr.priority = 0;
 
-	// Find the less prioritized node (including parenthesis)
-	for ( int i = 0; (*s)[i]; i++ ) {
-
-		if ( (*s)[i] == '(' ){
-			depth++;
+	for ( int i = 0; s[i]; i++ )
+	{
+		if ( s[i] == '(' )
+		{
+			curr.depth++;
 			continue;
 		}
-		else if ( (*s)[i] == ')' ) {
-			depth--;
+		else if ( s[i] == ')' )
+		{
+			curr.depth--;
 			continue;
 		}
 
-		curr.type = get_type((*s)[i]);
-		curr.priority = CALC_PRIORITY(curr.type, depth);
-		curr.ptr = &(*s)[i];
-
-		if ( curr.type != -1 && curr.priority < min_prior.priority )
+		curr.ptr = &s[i];
+		if ( s[i] == '+' )
+			curr.weight = ADD;
+		else if ( s[i] == '*' )
+			curr.weight = MULTI;
+		else if ( isdigit(s[i]) )
+		{
+			curr.weight = VAL;
+			curr.val = s[i] - '0';
+		}
+		curr.priority = GET_PRIOR(curr.weight, curr.depth);
+		if ( curr.priority < min_prior.priority )
 			min_prior = curr;
 	}
+	if ( min_prior.weight == VAL )
+		return min_prior.val;
+	if (min_prior.ptr)
+		*min_prior.ptr = '\0';
+	if ( min_prior.weight == ADD )
+		return ( n(s) + n(min_prior.ptr + 1) );
+	else if ( min_prior.weight == MULTI )
+		return ( n(s) * n(min_prior.ptr + 1) );
+	return 0;
+}
 
-	new.type = min_prior.type;
-	if ( new.type == VAL ) {
-		new.val = min_prior.ptr[0] - '0';
-		return (new_node(new));
+bool check_set(char c, char *set)
+{
+	for (int i = 0; set[i]; i++)
+		if (c == set[i])
+			return true;
+	return false;
+}
+
+int validate_input(char *s)
+{
+	enum { ERR = -1, NICE = 0 };
+
+	if ( !s )
+		return ERR;
+
+	int d = 0;
+
+	for ( int i = 0; s[i]; i++ ) {
+		if ( s[i] == '(' )
+			d++;
+		else if ( s[i] == ')' )
+			d--;
+		if ( d < 0 )
+			return ERR;
+		if ( s[i] == '(' )
+		{
+			if ( !s[i+1] || !check_set(s[i+1], "()0123456789") )
+				return ERR;		
+		}
+		else if ( s[i] == ')' )
+		{
+			if ( s[i+1] && !check_set(s[i+1], ")*+") )
+				return ERR;		
+		}
+		else if ( check_set(s[i], "+*"))
+		{
+			if ( !s[i+1] || !check_set(s[i+1], "(0123456789") )
+				return ERR;
+		}
+		else if ( isdigit(s[i]) )
+		{
+			if ( s[i+1] && !check_set(s[i+1], "*+)") )
+				return ERR;
+		}
+		else {
+			return ERR;
+		}
 	}
-	*min_prior.ptr = 0;
-	char *right_s = min_prior.ptr + 1;
-	if ( right_s )
-		new.r = n(&right_s);
-	new.l = n(s);
-	
-	return new_node(new);
+	return NICE;
 }
 
 int main(int argc, char **argv)
 {
     if (argc != 2)
         return (1);
-	// if ( validate_input(argv[1]) == ERR )
-	// 	return ERR;
-    node *tree = n(&argv[1]);//parse_expr(argv[1]);
-    if (!tree)
-        return (1);
-    printf("%d\n", eval_tree(tree));
-    destroy_tree(tree);
+	if ( validate_input(argv[1]) == -1 )
+		return(1);
+	printf("%d\n", n(argv[1]));
+    // node *tree = parse_expr(argv[1]);
+    // if (!tree)
+    //     return (1);
+    // printf("%d\n", eval_tree(tree));
+    // destroy_tree(tree);
 }
